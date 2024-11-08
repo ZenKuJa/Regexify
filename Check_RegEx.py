1b4588d11bbf5be5c79b654e98667295bf1bc078
ZenKuJa <157032921+ZenKuJa@users.noreply.github.com>
2024-11-08 11:50:17 +0100

    Delete Check_RegEx.py from main

--- Check_RegEx.py
+++
@@ -1,9 +0,0 @@
-import re
-
-text = "david@gmail.com; nischal@web.de"
-pattern = r"^[a-zA-Z0-9\-]+@[a-zA-Z]+\.(com|net|org)$"
-
-if re.search(pattern, text):
-  print("Stimmt überein!")
-else:
-  print("Stimmt nicht überein!")
