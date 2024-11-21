1c1e016b61eeffe778bd90eb93771f108b6dd726
daviid.mth <82279956+xdave-mth@users.noreply.github.com>
2024-11-21 14:10:19 +0100

    added test cases

--- LLMRegExGenerator.py
+++ LLMRegExGenerator.py
@@ -10,6 +10,7 @@ class LLMRegExGenerator:
   # Funktion, welche den Userinput zusammen mit den Promptanweisungen an das LLM sendet und die Antwort als String zurückgibt
   def generateRegEx(self, inputList):
     genai.configure(api_key=self.google_API_KEY)
+    
     # Parameter zum Fine Tuning des Modells 
     generation_config = {
       "temperature": 0.3,
@@ -18,15 +19,16 @@ class LLMRegExGenerator:
       "max_output_tokens": 256,
       "response_mime_type": "text/plain",
     }
+    
     # Modellauswahl und Anweisungen
     model = genai.GenerativeModel(
       model_name="gemini-1.5-pro",
       generation_config=generation_config,
       system_instruction= open('LLMRegExGeneratorInstructions.txt','r').read())
 
+    #Antwort generieren
     response = model.generate_content("Hier sind die Input Strings: " + " ; ".join(inputList))
     return response.text
-
 
 
   # Validiert den erzeugten RegEx, ob er korrekt ist
